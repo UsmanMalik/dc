f5b8b289ed83c9758b56d77c0ac85967922b1d83
Usman Malik <malik.usman.ek@gmail.com>
2019-02-23 02:36:23 +0400

    adding dummy root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :data_collect do
+    
+  end
 end
