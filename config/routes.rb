0af822efac1a89e142d65bd2281e8cf164b17853
Usman Malik <malik.usman.ek@gmail.com>
2019-02-23 02:46:07 +0400

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  root "products#index"
+  resources :product
+#  Main static website
   namespace :data_collect do
-    
+
   end
 end
